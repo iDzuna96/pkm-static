a3d23e1f9feb2ca048ae6d1543dec556edfcfdea
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2019-08-18 21:40:51 +0800

    start

---
+++ precache-manifest.4603e400a29398f24f5e6a7d08ae593b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e02d69e384a635a56a23",
+    "url": "/static/css/main.16ac1a31.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "6c9776e5447aecac2db5",
+    "url": "/static/js/2.c196b121.chunk.js"
+  },
+  {
+    "revision": "e02d69e384a635a56a23",
+    "url": "/static/js/main.b9c75eb3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "6c9776e5447aecac2db5",
+    "url": "/static/css/2.6763acfd.chunk.css"
+  },
+  {
+    "revision": "7dae7ae8717707813321617623f50243",
+    "url": "/index.html"
+  }
+];
